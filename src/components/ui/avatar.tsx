60b55ad48981e9ffcd89183091b0c2286648e7cc
Roy Palnadi <revannaldi45@gmail.com>
2025-06-01 23:07:57 +0700

    initial

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -12,18 +12,19 @@ export default function Avatar({ name = '', image }: { name: string; image?: str
 
     return (
         <div className="w-12 h-12 rounded-full relative overflow-hidden bg-gradient-to-br from-emerald-400 to-teal-500 flex items-center justify-center text-white text-lg">
-            {!image || !imgError
-                ? // <Image
-                  //     src={image ?? ''}
-                  //     alt={name}
-                  //     layout="fill"
-                  //     objectFit="cover"
-                  //     onError={() => setImgError(true)}
-                  //     loading="lazy"
-                  //     decoding="async"
-                  // />
-                  initials
-                : initials}
+            {!image || !imgError ? (
+                <Image
+                    src={image ?? ''}
+                    alt={name}
+                    layout="fill"
+                    objectFit="cover"
+                    onError={() => setImgError(true)}
+                    loading="lazy"
+                    decoding="async"
+                />
+            ) : (
+                initials
+            )}
         </div>
     );
 }
